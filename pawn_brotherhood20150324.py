12435423c4c342c4135189427a6c0502f856ba36
Spencer Zhang <spencerpomme@live.com>
2015-04-23 19:45:11 +0800

    Solved pawn brotherhood problem.

--- pawn_brotherhood20150324.py
+++ pawn_brotherhood20150324.py
@@ -1,6 +1,16 @@
 def safe(pawns):
-    state_list = [(x,y,0) for x in range(8) for y in range(8)]
-    print(state_list)
-
-
-safe({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})
+    match_dict = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
+    coords = []
+    safe_pawn = 0
+    for pawn in pawns:        
+        coords.append((match_dict[pawn[0]],int(pawn[1])))
+    for c in coords:
+        guard_c1 = (c[0]-1, c[1]-1)
+        guard_c2 = (c[0]+1, c[1]-1)
+        if (guard_c1 in coords) or (guard_c2 in coords):
+            safe_pawn += 1
+    return safe_pawn
+            
+    
+test = safe({'a1','b2','c2','c3','d2','d1'})
+print(test)
