eeb790d57e5a6c99dbf020f1dbc65d1cbed0fb48
spencerpomme <spencerpomme@live.com>
2015-05-14 00:32:46 +0800

    Learning Python textbook exercise

---
+++ person.py
@@ -0,0 +1,53 @@
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def giveRaise(self, percent):
+        self.pay = int(self.pay *(1 + percent))
+
+    def __repr__(self):
+        return '[%s: %s]' % (self.__class__.__name__, self.pay)
+
+
+class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'mgr', pay)
+        
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent+bonus)
+
+
+class Department:
+    def __init__(self, *args):
+        self.members = list(args)
+
+    def addMember(self, person):
+        self.members.append(person)
+
+    def giveRaise(self, percent):
+        for person in self.members:
+            person.giveRaise(percent)
+
+    def showAll(self):
+        for person in self.members:
+            print(person)
+
+
+
+bob = Person('Bob Smith')
+sue = Person('Sue Jones', job='dev', pay=100000)
+tom = Manager('Tom Jones', pay=50000)
+
+if __name__ == '__main__':
+    
+    development = Department(bob, sue)
+    development.addMember(tom)
+    development.giveRaise(.10)
+    development.showAll()
+    
+    
