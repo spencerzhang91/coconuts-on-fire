54c8894b1a94ecf053d2888aa3a77f68ba5996f2
spencerpomme <spencerpomme@live.com>
2015-05-14 15:07:49 +0800

    using pickle to store class instances

---
+++ makedb.py
@@ -0,0 +1,12 @@
+from person import Person, Manager
+bob = Person('Bob Clark')
+sue = Person('Sue Stark', job='dev', pay=100000)
+tom = Manager('Tom Jones', 50000)
+
+import shelve
+db = shelve.open('persondb')
+for obj in (bob, sue, tom):
+    db[obj.name] = obj
+db.close()
+
+
