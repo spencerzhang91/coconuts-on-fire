82de134b022afd73ffde7ceb365759ccd1f93aed
Spencer Zhang <spencerpomme@live.com>
2015-06-12 10:47:45 +0800

    chap 5

---
+++ golf.c
@@ -0,0 +1,10 @@
+/* golf.c -- golf championship score card */
+#include <stdio.h>
+int main(void)
+{
+	int jane, tarzan, cheeta;
+	cheeta = tarzan = jane = 68;
+	printf("                  jane  tarzan  cheeta\n");
+	printf("First round score %4d %8d %8d\n", jane, tarzan, cheeta);
+	return 0;
+}
