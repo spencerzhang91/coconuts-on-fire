022691ac1bb6cb4ebc32fabbc61d50df63a6cbb7
Spencer Zhang <spencerpomme@live.com>
2015-06-12 10:55:24 +0800

    ...

--- golf.c
+++ golf.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int jane, tarzan, cheeta;
 	cheeta = tarzan = jane = 68;
-	printf("                  jane  tarzan  cheeta\n");
+	printf("%22s %8s %8s\n", "jane", "tarzan", "cheeta");
 	printf("First round score %4d %8d %8d\n", jane, tarzan, cheeta);
 	return 0;
 }
