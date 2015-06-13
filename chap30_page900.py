0fd4be5125a27c9f90a4b0da026a0accfaf58760
Spencer Zhang <spencerpomme@live.com>
2015-06-13 15:56:53 +0800

    chap 30

---
+++ chap30_page900.py
@@ -0,0 +1,36 @@
+#!python3
+# file skipper.py
+# Learing about 'user-defined iteralble', and frankly speaking, I'm confused...
+
+class SkipObject:
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __iter__(self):
+        return SkipIterator(self.wrapped)
+        
+
+class SkipIterator:
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.offset = 0
+
+    def __next__(self):
+        if self.offset >= len(self.wrapped):
+            raise StopIteration
+        else:
+            item = self.wrapped[self.offset]
+            self.offset += 2
+            return item
+
+if __name__ == '__main__':
+    alpha = 'abcdef'
+    skipper = SkipObject(alpha)
+    I = iter(skipper)
+    print(next(I), next(I), next(I))
+
+    for x in skipper:
+        for y in skipper:
+            print(x + y, end = ' ')
+
+            
