f24c61e9c001143f4734aadb908c7f2a3843699d
Spencer Zhang <spencerpomme@live.com>
2015-06-14 18:54:10 +0800

    chap 30
    a class as a practise of the concept of operator overloading.

---
+++ class_vector.py
@@ -0,0 +1,25 @@
+class Vector:
+    def __init__(self, x, y):
+        self.vertax = (x, y)
+
+    def __getitem__(self, index):
+        return self.vertax[index]
+        
+    def __add__(self, other):
+        return Vector(self.vertax[0] + other[0], self.vertax[1] + other[1])
+
+    def __repr__(self):
+        return str(self.vertax)
+
+
+a = Vector(1,1)
+b = Vector(2,1)
+print(a[0])
+print(a)
+print(b)
+c = a + (3,3)
+print(c)
+print(type(c))
+d = c + a
+print(d)
+    
