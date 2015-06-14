e55ce8ee4084811dc4687943b09f914b7ef88666
Spencer Zhang <spencerpomme@live.com>
2015-06-14 19:20:31 +0800

    ...

--- class_vector.py
+++ class_vector.py
@@ -1,9 +1,12 @@
 class Vector:
     def __init__(self, x, y):
-        self.vertax = (x, y)
+        self.vertax = [x, y]
 
     def __getitem__(self, index):
         return self.vertax[index]
+
+    def __setitem__(self, index, value):
+        self.vertax[index] = value
         
     def __add__(self, other):
         return Vector(self.vertax[0] + other[0], self.vertax[1] + other[1])
@@ -17,9 +20,11 @@ b = Vector(2,1)
 print(a[0])
 print(a)
 print(b)
-c = a + (3,3)
+c = a + [3,3]
 print(c)
 print(type(c))
 d = c + a
 print(d)
+a[1] = 3
+print(a)
     
