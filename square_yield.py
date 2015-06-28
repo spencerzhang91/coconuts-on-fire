e561404637a56cd2c47eb376dd254293bcdfb57a
Spencer Zhang <spencerpomme@live.com>
2015-06-28 23:37:36 +0800

    ...

---
+++ square_yield.py
@@ -0,0 +1,14 @@
+# file squares_yield.py
+
+class Squares:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        for value in range(self.start, self.stop+1):
+            yield value ** 2
+
+if __name__ == '__main__':
+    for i in Squares(1,5):
+        print(i, end=' ')
