381f37ad3c80989cba306c8e3279a520c3f2db9c
Spencer Zhang <spencerpomme@live.com>
2015-06-29 12:46:02 +0800

    ...

---
+++ contains.py
@@ -0,0 +1,43 @@
+# file contains.py of Learing Python(5th Edition) P907
+
+class Iters:
+    def __init__(self, value):
+        self.data = value
+
+    def __getitem__(self, i):
+        print('get[%s]:' % i, end=' ')
+        return sefl.data[i]
+
+    def __iter__(self):
+        print('iter=> ', end=' ')
+        self.ix = 0
+        return self
+
+    def __next__(self):
+        print('next:', end=' ')
+        if self.ix == len(self.data):
+            raise StopIteration
+        item = self.data[self.ix]
+        self.ix += 1
+        return item
+
+    def __contains__(self, x):
+        print('contains: ', end=' ')
+        return x in self.data
+
+if __name__ == '__main__':
+    X = Iters([1,2,3,4,5])
+    print(3 in X)
+    for i in X:
+        print(i, end=' | ')
+
+    print()
+    print([i ** 2 for i in X])
+    print(list(map(bin, X)))
+
+    I = iter(X)
+    while True:
+        try:
+            print(next(I), end=' @ ')
+        except StopIteration:
+            break
