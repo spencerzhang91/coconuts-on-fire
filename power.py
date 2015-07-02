7da950daeb6d667b5aa6b18195f97fab3e561ef9
Spencer Zhang <spencerpomme@live.com>
2015-07-02 10:42:17 +0800

    simplify

--- power.py
+++ power.py
@@ -4,9 +4,8 @@ def main():
         while 1:
             x = float(input('x = '))
             exp = int(input('exp = '))
-            print(x ** exp)
-    except:
-        print('end')
-    
+            print("%.3f to the power %d is %.5f\n" % (x,exp,x ** exp))
+            print("Please enter next pair or q to quit.")
+    except:print('Hope you enjoyed the power trip...')    
 main()
         
