90e8711d1e7a9b14cc0517a8948d0b18ea2cbc15
Spencer Zhang <spencerpomme@live.com>
2015-07-06 09:54:11 +0800

    ...

--- DSA/linked_list_node.py
+++ DSA/linked_list_node.py
@@ -1,18 +1,41 @@
+# a linked list(here the singly linked list) must have a head reference(pointer), otherwise it would
+# be impossible to locate the head or any other node.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
-head = ListNode(3)
-mid = ListNode(34)
-tail = ListNode(4)
-head.next = mid
-mid.next = tail
-mid = tail = None
-print(head.val)
-print(head.next.val)
-print(head.next.next.val)
+    def __repr__(self):
+        return str(self.val)
+
+def traverse(head):
+    curNode = head
+    while curNode != None:
+        print(curNode)
+        curNode = curNode.next
+
+def unorderedSearch(head, target):
+    curNode = head
+    while curNode != None and curNode.val != target:
+        curNode = curNode.next
+    return curNode is not None
+    
 
 
-# a linked list(here the singly linked list) must have a head reference(pointer), otherwise it would
-# be impossible to locate the head or any other node.
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+e = ListNode(5)
+f = None
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+b = c = d = e = None
+#print(a)
+
+traverse(a)
+print(unorderedSearch(a, 3))
