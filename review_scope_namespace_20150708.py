7f8abefa8e5f82c82bc7c4ba0e6856ac577f578b
Spencer Zhang <spencerpomme@live.com>
2015-07-08 19:34:58 +0800

    ....

--- review_scope_namespace_20150708.py
+++ review_scope_namespace_20150708.py
@@ -1,47 +1,34 @@
 class Alter1:
     def half(self, array, k):
-        print('A1: before:', array)
-        array = array[k:] + array[:k]
-        
-        
-        print('A1: after:', array, '\n')
+        array = array[k:] + array[:k]      
+        print('l1 inside function:', id(array))
         
 
 class Alter2:
     def half(self, array, k):
-        print('A2: array:', array)
         temp = array[k:] + array[:k]
-        print('A2: temp:', temp)
+        
         for i in range(len(array)):
             array[i] = temp[i]
-        print('A2: array:', array, '\n')
-        
+        print('l2 inside function:', id(array))
 
+
+        
 l1 = [1,2,3,4]
 l2 = [5,6,7,8]
-l3 = [11,22,33,44]
-l4 = [55,66,77,88]
 
+print('l1 Before alter:', l1, '; l1 id outside function call:', id(l1))
+print('l2 Before alter:', l2, '; l2 id outside function call:', id(l2))
+print('\n')
 test1 = Alter1()
 test2 = Alter2()
-test3 = Alter1
-tobj3 = Alter1()
-test4 = Alter2
-tobj4 = Alter2()
 
 f1 = test1.half
 f2 = test2.half
-f3 = test3.half
-f4 = test4.half
 
-print(type(f1), type(f2), type(f3), type(f4))
 f1(l1, 2)
 f2(l2, 2)
-f3(tobj3, l3, 2)
-f4(tobj4, l4, 2)
-
-print('f1 ->', l1)
-print('f2 ->', l2)
-print('f3 ->', l3)
-print('f4 ->', l4)
+print('*' * 20, '\n')
+print('l1 after alter:', l1)
+print('l2 after alter:', l2)
 
