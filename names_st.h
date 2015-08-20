9126a41299808d0da5245ae09a5f743ea5634a51
Spencer Zhang <spencerpomme@live.com>
2015-08-20 16:12:44 +0800

    duplicate defination protection

--- names_st.h
+++ names_st.h
@@ -1,4 +1,6 @@
 // names_st.h -- headfile of names_st 
+#ifndef NAMES_H_
+#define NAMES_H_
 // constants
 #define SLEN 32
 // structure declairation
@@ -13,4 +15,4 @@ typedef struct names_st names;
 // function prototype
 void get_names(names *);
 void show_names(const names *);
-
+#endif
