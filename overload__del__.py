86e75705bb1d5655f78bbd7c1e90cbe0762f0d85
Spencer Zhang <spencerpomme@live.com>
2015-11-18 12:38:31 +0800

    NOV18

---
+++ overload__del__.py
@@ -0,0 +1,14 @@
+class Life:
+    def __init__(self, name):
+        print('Hello, %s' % name)
+        self.name = name
+    def live(self):
+        print(self.name)
+
+    def __del__(self):
+        print('Goodbye, %s' % self.name)
+
+if __name__ == "__main__":
+    brain = Life('Brain')
+    brain.live()
+    sue = Life('Sue')
