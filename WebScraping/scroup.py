a2a142b7c850dbfbca52d2d7631dbead54e1dfb0
Spencer Zhang <spencerpomme@live.com>
2015-12-24 14:06:23 +0800

    new project

---
+++ WebScraping/scroup.py
@@ -0,0 +1,19 @@
+#! python3
+# This script is created for scraping douban group topics
+import requests, bs4
+num = 0
+url = 'http://www.douban.com/group/gz/discussion?start=' + str(num)
+res = requests.get(url)
+try:
+    res.raise_for_status()
+except Exception as e:
+    print('There is a problem:', e)
+'''
+print(type(res))
+page = open('topics.html', 'wb')
+for chunk in res.iter_content(100000):
+    page.write(chunk)
+'''
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
