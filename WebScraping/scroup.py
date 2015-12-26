c62777119a668fd756ee56529e2b05c7ba53aeab
Spencer Zhang <spencerpomme@live.com>
2015-12-26 17:32:52 +0800

    DEC26
    new problem occured

--- WebScraping/scroup.py
+++ WebScraping/scroup.py
@@ -26,8 +26,8 @@ def StartOperation(init_url: str, pages: int) -> None:
     error_counter = 0
     perpage = 25
     failure_urls = []
-    test_file = open(r'C:\Users\spencer\Desktop\guangzhouzufang.csv', 'w', newline='')
-    test_writer = csv.writer(test_file)
+    file = open(r'C:\Users\spencer\Desktop\guangzhouzufang.csv', 'w', newline='')
+    writer = csv.writer(file)
     for i in range(pages):
         print('Scraping page %d...' % (i+1))
         url = init_url + str(num + perpage * i)
@@ -45,14 +45,15 @@ def StartOperation(init_url: str, pages: int) -> None:
         
         print('[page info]titles: %d authors: %d follows: %d lastres: %d' %
               (len(titles), len(authors), len(follows), len(lastres)))      
-        print('Saving page %d to cwd local file %s...' % (i+1, 'test.csv'))
+        print('Saving page %d to cwd local file %s...' % (i+1, 'guangzhouzufang.csv'))
         
         for j in range(len(titles)):
             try:
-                test_writer.writerow([titles[j]['title'].encode('utf-8'),
-                                      authors[j].getText().encode('utf-8'),
-                                      follows[j].getText().encode('utf-8'),
-                                      lastres[j].getText().encode('utf-8')])
+                # the four lines below occur encoding error, help!
+                writer.writerow([titles[j]['title'].encode(),
+                                 authors[j].getText().encode(),
+                                 follows[j].getText().encode(),
+                                 lastres[j].getText().encode()])
                 # print(titles[j]['title'])
             except Exception as e:
                 print('Error occured on page %d line %d' % (i+1, j+1))
@@ -61,7 +62,7 @@ def StartOperation(init_url: str, pages: int) -> None:
                 if url not in failure_urls:
                         failure_urls.append(url)
                 error_counter += 1     
-    test_file.close()
+    file.close()
     print('\nTotal failed topic number: %d topics!' % error_counter)
     return failure_urls
 
@@ -69,8 +70,11 @@ if __name__ == '__main__':
     url_list = ['https://www.douban.com/group/tianhezufang/discussion?start=',
                 'http://www.douban.com/group/gz020/discussion?start=']
     url = url_list[1]
-    failures = StartOperation(url, 1322)
-    print('[The urls below occured problem]:')
-    for url in failures:
-        print(url)
+    failures = StartOperation(url, 5)
+    if failures:
+        print('[The urls below occured problem]:')
+        for url in failures:
+            print(url)
+    else:
+        print('All pages successfully scraped!')
 
