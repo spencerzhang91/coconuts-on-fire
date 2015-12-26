4f18bb7042c6cd1672296ed8d481680dc223a804
Spencer Zhang <spencerpomme@live.com>
2015-12-26 16:00:11 +0800

    DEC26
    previous two problems solved

--- WebScraping/scroup.py
+++ WebScraping/scroup.py
@@ -17,20 +17,6 @@ def showfunc(l1, l2, l3, l4):
                l4[i].getText()))
 
 
-def saveLocal(col1, col2, col3, col4):
-    '''
-    type cols: list
-    This function saves the scraped data into CSV file
-    '''
-    local_file = open('topic_saver.csv', 'w')
-    file_writer = csv.writer(local_file)
-    for i in len(col1):
-        file_writer.writerow([col1[i],
-                              col2[i],
-                              col3[i],
-                              col4[i]])
-
-
 def StartOperation(init_url: str, pages: int) -> None:
     '''
     type init_url: str
@@ -53,29 +39,29 @@ def StartOperation(init_url: str, pages: int) -> None:
     
         soup = bs4.BeautifulSoup(res.text, 'lxml')
         titles = soup.select('tr[class] > td[class="title"] > a[class=""]')
-        # need to be modified: retrive the attribute value of tag <td>
         authors = soup.select('tr[class] > td[nowrap="nowrap"] > a[class=""]')
         follows = soup.select('tr[class] > td[class=""]')
         lastres = soup.select('tr[class] > td[class="time"]')
+        
         print('[page info]titles: %d authors: %d follows: %d lastres: %d' %
-              (len(titles), len(authors), len(follows), len(lastres)))
-        # showfunc(titles, authors, follows, lastres)
+              (len(titles), len(authors), len(follows), len(lastres)))      
         print('Saving page %d to cwd local file %s...' % (i+1, 'test.csv'))
+        
         for j in range(len(titles)):
             try:
-                test_writer.writerow([titles[j].getText().encode('utf-8'),
+                test_writer.writerow([titles[j]['title'].encode('utf-8'),
                                       authors[j].getText().encode('utf-8'),
                                       follows[j].getText().encode('utf-8'),
                                       lastres[j].getText().encode('utf-8')])
+                print(titles[j]['title'])
             except Exception as e:
                 print('Error occured on page %d line %d' % (i+1, j+1))
                 print(*[titles[j].getText(), authors[j].getText(),
-                       follows[j].getText(), lastres[j].getText()])
-                # try to find out why failure(1) happens
                 print('error message:', e)
                 if url not in failure_urls:
                     failure_urls.append(url)
                 error_counter += 1
+                        
     test_file.close()
     print('\nTotal failed topic number: %d topics!' % error_counter)
     return failure_urls
