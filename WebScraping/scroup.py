87a294b93470949a45eb2f542d3515cb57d2bfc6
Spencer Zhang <spencerpomme@live.com>
2015-12-28 12:51:01 +0800

    DEC28
    ...

--- WebScraping/scroup.py
+++ WebScraping/scroup.py
@@ -3,7 +3,14 @@
 # Should be able to scrape [topic,author,follow,lastresponse]
 # info of all group topics
 import requests, bs4, csv
+class CSVfileNameError(Exception):
+    def __str__(self):
+        return 'Invalid file name, please add .csv at the end of name.'
 
+class WrongURL(Exception):
+    def __str__(self):
+        return 'Invalid url input, input should be valid url of douban group.'
+    
 def showfunc(l1, l2, l3, l4):
     '''
     To print out the scraped topics
@@ -21,17 +28,35 @@ def Initialization():
     This function asks user to input the needed initialization
     informations of the scraping mission.
     '''
-    url = input('Please copy the url here:')
+    url = pagenum = filename = None
     while True:
-        try:
-            pagenum = int(input('Please enter the page numbers you want'
-                            'to scrape:'))
+        if not url:
+            try:
+                url = input('Please copy the url here(drop num after"="):')
+            except WrongURL as wurl:
+                print(wurl)
+                url = None
+                continue
+        if not pagenum:
+            try:
+                pagenum = int(input('Please enter the page numbers you want'
+                                ' to scrape:'))
+            except ValueError:
+                print('Please enter an integer!')
+                pagenum = None
+                continue
+        if not filename:
+            try:
+                filename = input('Please enter the file name to save data'
+                         '(file name must include postfix ".csv"):')
+                if filename[-4:] != '.csv':
+                    raise CSVfileNameError
+            except CSVfileNameError as cfne:
+                print(cfne)
+                filename = None
+                continue
+        if url and pagenum and filename:
             break
-        except ValueError as a:
-            print(a)
-            print('Please enter an integer!')
-        
-    filename = input('Please enter the file name to save data:')
     return (url, pagenum, filename)
 
 
@@ -44,7 +69,7 @@ def StartOperation(init_url: str, pages: int, filename: str)->None:
     error_counter = 0
     perpage = 25
     failure_urls = []
-    file = open(r'C:\Users\spencer\Desktop\%s.csv' % filename,
+    file = open(r'C:\Users\spencer\Desktop\%s' % filename,
                 'w', newline='',
                 encoding='utf8')
     writer = csv.writer(file)
@@ -65,7 +90,7 @@ def StartOperation(init_url: str, pages: int, filename: str)->None:
         
         print('[page info]titles: %d authors: %d follows: %d lastres: %d' %
               (len(titles), len(authors), len(follows), len(lastres)))      
-        print('Saving page %d to cwd local file %s.csv...' % (i+1, filename))
+        print('Saving page %d to cwd local file %s...' % (i+1, filename))
         
         for j in range(len(titles)):
             try:
@@ -88,14 +113,17 @@ def StartOperation(init_url: str, pages: int, filename: str)->None:
     return failure_urls
 
 if __name__ == '__main__':
+    """
     url_list = ['https://www.douban.com/group/tianhezufang/discussion?start=',
                 'http://www.douban.com/group/gz020/discussion?start=',
                 'http://www.douban.com/group/kaopulove/discussion?start=']
     url = url_list[2]
-    failures = StartOperation(url, 1, 'gouda')
+    """
+    url, pgm, fln = Initialization()
+    failures = StartOperation(url, pgm, fln)
     if failures:
         print('[The urls below occured problem]:')
-        for url in failures:
-            print(url)
+        for item in failures:
+            print(item)
     else:
         print('All pages successfully scraped!')
