e220a400ab62435cf67cad9dbe6be68ebfbf435c
Spencer Zhang <spencerpomme@live.com>
2016-02-03 22:08:45 +0800

    FEB03

--- regression_factor_select.R
+++ regression_factor_select.R
@@ -1,1 +1,12 @@
 # determine the proper factors of a regression model
+combinations <- function(ivnum)
+{
+    count <- 0
+    for (i in ivnum)
+    {
+        n <- combn(ivnum, i)
+        count <- count + ncol(n)
+        print(count)
+    }
+    return(count)
+}
