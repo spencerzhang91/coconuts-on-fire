9ec35e3bd25e5be4147830c865ab8afa53595f86
spencerpomme <spencerpomme@live.com>
2016-03-08 11:32:59 +0800

    MAR08

--- baidu_API.py
+++ baidu_API.py
@@ -6,9 +6,10 @@ This API can be called maximum 2000 times per day.
 """
 
 import requests, json
+import psycopg2
 
 mykey = "IniXfqhsWAyZQpkmh5FtEVv0" # my developer key
-city = "北京"
+city = "韶关"
 place = "公园"
 
 payload_city = {'ak': mykey,
@@ -17,10 +18,14 @@ payload_city = {'ak': mykey,
             'page_size': 10,
             'page_num': 0,
             'scope': 2,
-            'regrion': city}
+            'region': city}
 
 basicAPI = "http://api.map.baidu.com/place/v2/search"
-test = "http://api.map.baidu.com/place/v2/search?query=百度大厦&region=深圳&city_limit=true&output=json&ak=IniXfqhsWAyZQpkmh5FtEVv0"
+test = "http://api.map.baidu.com/place/v2/search?ak=IniXfqhsWAyZQpkmh5FtEVv0&output=json&query=%E9%93%B6%E8%A1%8C&page_size=10&page_num=0&scope=2&region=%E5%8C%97%E4%BA%AC"
+
 res = requests.get(basicAPI, params=payload_city)
-print(res.url)
-print(res.text)
+jsonobj = json.loads(res.text)
+print(type(jsonobj))
+print(type(res.text))
+print(json.dumps(jsonobj, sort_keys=False, indent=4))
+
