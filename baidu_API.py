23669755d03a5198ecfdde2cf7877051472e8100
spencerpomme <spencerpomme@live.com>
2016-03-08 13:00:44 +0800

    add three methods

--- baidu_API.py
+++ baidu_API.py
@@ -1,4 +1,3 @@
-
 """
 A simple script that uses Baidu Place API to search certain kinds of place
 in a range of circular space.
@@ -12,16 +11,21 @@ mykey = "IniXfqhsWAyZQpkmh5FtEVv0" # my developer key
 city = "韶关"
 place = "公园"
 
-payload_city = {'ak': mykey,
-            'output': 'json',
-            'query': place,
-            'page_size': 10,
-            'page_num': 0,
-            'scope': 2,
-            'region': city}
+payload_city = {
+    'ak': mykey,
+    'output': 'json',
+    'query': place,
+    'page_size': 10,
+    'page_num': 0,
+    'scope': 2,
+    'region': city
+}
 
+payload_circ = {
+    'ak': mykey,
+
+}
 basicAPI = "http://api.map.baidu.com/place/v2/search"
-test = "http://api.map.baidu.com/place/v2/search?ak=IniXfqhsWAyZQpkmh5FtEVv0&output=json&query=%E9%93%B6%E8%A1%8C&page_size=10&page_num=0&scope=2&region=%E5%8C%97%E4%BA%AC"
 
 res = requests.get(basicAPI, params=payload_city)
 jsonobj = json.loads(res.text)
