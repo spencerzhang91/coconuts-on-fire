45483f5438ec83210eeb49694090dae6f9e7017e
spencerpomme <spencerpomme@live.com>
2016-03-08 16:53:09 +0800

    delete duplicate file

--- baidu_API.py
+++ baidu_API.py
@@ -11,8 +11,8 @@ mykey = "IniXfqhsWAyZQpkmh5FtEVv0" # my developer key
 city = "韶关"
 place = "公园"
 
-cord1 = (39.915, 116.404)
-cord2 = (39.975, 116.414)
+coor1 = (39.915, 116.404)
+coor2 = (39.975, 116.414)
 radius = 500 # meters
 
 payload_city = {
@@ -32,8 +32,8 @@ payload_rect = {
     'page_size': 10,
     'page_num': 0,
     'scope': 2,
-    'bounds': "%s, %s, %s, %s" % (*cord1, *cord2),
-    'location': cord1,
+    'bounds': "%s, %s, %s, %s" % (*coor1, *coor2),
+    'location': coor1,
     'radius': radius
 }
 
@@ -57,4 +57,11 @@ res_circ = requests.get(baseAPI, params=payload_circ)
 jsonobj = json.loads(res_city.text)
 print(type(jsonobj))
 print(type(res_city.text))
-print(json.dumps(jsonobj, sort_keys=False, indent=4))
+# print(json.dumps(jsonobj, sort_keys=False, indent=4))
+
+# Below this line defines a series of Baidu geo-data API calling functions
+def addr2corr(address: str)->tuple:
+    '''
+    This function converts address to a (longitude, latitude) coordinate.
+    '''
+    raise NotImplementedError
