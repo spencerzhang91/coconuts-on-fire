40f10918f785b000f82ff5676d30400b7015f9df
spencerpomme <spencerpomme@live.com>
2016-03-08 13:03:54 +0800

    Three methods

--- baidu_API.py
+++ baidu_API.py
@@ -11,6 +11,10 @@ mykey = "IniXfqhsWAyZQpkmh5FtEVv0" # my developer key
 city = "韶关"
 place = "公园"
 
+cord1 = (39.915, 116.404)
+cord2 = (39.975, 116.414)
+radius = 500 # meters
+
 payload_city = {
     'ak': mykey,
     'output': 'json',
@@ -21,10 +25,27 @@ payload_city = {
     'region': city
 }
 
+payload_rect = {
+    'ak': mykey,
+    'output': json,
+    'query': place,
+    'page_size': 10,
+    'page_num': 0,
+    'scope': 2,
+    'bounds': "%s, %s, %s, %s" % (*cord1, *cord2)
+    'location': cord1,
+    'radius': radius
+}
+
 payload_circ = {
     'ak': mykey,
-
+    'output': json,
+    'query': place,
+    'page_size': 10,
+    'page_num': 0,
+    'scope': 2,
 }
+
 basicAPI = "http://api.map.baidu.com/place/v2/search"
 
 res = requests.get(basicAPI, params=payload_city)
@@ -32,4 +53,3 @@ jsonobj = json.loads(res.text)
 print(type(jsonobj))
 print(type(res.text))
 print(json.dumps(jsonobj, sort_keys=False, indent=4))
-
