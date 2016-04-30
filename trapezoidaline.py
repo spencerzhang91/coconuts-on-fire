e7b0ad0b08e34390e60fae00f6658135f180e64d
spencerpomme <spencerpomme@live.com>
2016-04-30 17:15:20 +0800

    APL30

--- trapezoidaline.py
+++ trapezoidaline.py
@@ -1,6 +1,8 @@
-class Point:
+import math
+
+class Circle:
     """
-    A point class.
+    A circle class.
     """
     def __init__(self, x, y, r):
         self.x = x
@@ -10,8 +12,12 @@ class Point:
     def __repr__(self):
         return "(%f %f %f)" % (x, y, r)
 
-def findPQ(circle1: Circle, circle2: Circle)->list:
+def findPQ(c1: Circle, c2: Circle)->list:
     """
     This function finds the P point and Q point of two circles.
     """
-    pass
+    sin_alpha = (c1.r - c2.r) / oDist(c1, c2)
+    cos_alpha = math.sqrt(oDist(c1, c2) - (c1.r - c2.r)**2) / oDist(c1, c2)
+    sin_theta = (c1.y - c2.y) / oDist(c1, c2)
+    cos_theta = (c1.x - c2.x) / oDist(c1, c2)
+
