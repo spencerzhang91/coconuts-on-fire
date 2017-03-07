f8b430182175278088cc7f0d7e6c1360f15b286a
spencerpomme <spencerpomme@live.com>
2017-03-07 22:45:38 +0800

    to be deleted

---
+++ DSA/zhihu_headers.py
@@ -0,0 +1,7 @@
+headers = {
+        'Cookie': '316558|dbebe0ac7c8cf0185517814d52954e37cb8eb2f7"',
+        'Host':'www.zhihu.com',
+        'Referer':'http://www.zhihu.com/',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+        'Accept-Encoding':'gzip'
+    }
