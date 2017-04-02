c9385135e99e33f473c46173e79052b26acc10d1
spencerpomme <spencerpomme@live.com>
2017-04-02 20:04:15 +0800

    fix regex bug
    with an ugly line

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -5,10 +5,14 @@ import re
 from subprocess import check_output, call
 
 output = check_output(['pip3', 'list', '--outdated'])
-outdated = re.findall('\w+(?= \(\w+)', str(output))
+outdated = re.findall('^(?=b)\S+(?= \(\w+)|(?<=\\\\n)\S+(?= \(\w+)', str(output))
+
+outdated[0] = outdated[0][2:] # This line is so ugly, but this is all I can do.
+
 for pkg in outdated:
     try:
         call(['pip3', 'install', pkg, '--no-cache-dir', '--upgrade'])
+        print('updating...')
     except Exception as e:
         print(e)
         print('Can not upgrade package %s' % pkg)
