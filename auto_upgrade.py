2e2fbde7e1db902f3f47e2f20550fd4a81a10c0c
spencerpomme <spencerpomme@live.com>
2017-04-29 11:47:07 +0800

    add condition when no updates

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -6,15 +6,17 @@ from subprocess import check_output, call
 
 output = check_output(['pip3', 'list', '--outdated'])
 outdated = re.findall('^(?=b)\S+(?= \(\w+)|(?<=\\\\n)\S+(?= \(\w+)', str(output))
+if not outdated:
+    print('Everything is up to date.')
+else:
+    outdated[0] = outdated[0][2:] # This line is so ugly, but this is all I can do.
 
-outdated[0] = outdated[0][2:] # This line is so ugly, but this is all I can do.
-
-for pkg in outdated:
-    try:
-        call(['pip3', 'install', pkg, '--no-cache-dir', '--upgrade'])
-        print('updating...')
-    except Exception as e:
-        print(e)
-        print('Can not upgrade package %s' % pkg)
-        continue
-print('Auto upgrade successful.')
+    for pkg in outdated:
+        try:
+            call(['pip3', 'install', pkg, '--no-cache-dir', '--upgrade'])
+            print('updating...')
+        except Exception as e:
+            print(e)
+            print('Can not upgrade package %s' % pkg)
+            continue
+    print('Auto upgrade successful.')
