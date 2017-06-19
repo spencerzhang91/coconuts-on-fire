98fb0c903ddfb4bc4cd173bce652cbaa88248a8a
spencerpomme <spencerpomme@live.com>
2017-06-19 10:56:57 +0800

    solve it once and for all

--- auto_upgrade.py
+++ auto_upgrade.py
@@ -1,23 +1,7 @@
 #! /usr/local/bin/python3
 # can not be used on windows due to line end difference.
 import pip
-import re
-from subprocess import check_output, call
+from subprocess import call
 
-output = check_output(['pip3', 'list', '--outdated']) # Now this line fails, why?
-outdated = re.findall('^(?=b)\S+(?= \(\w+)|(?<=\\\\n)\S+(?= \(\w+)', str(output))
-print('hh')
-if not outdated:
-    print('Everything is up to date.')
-else:
-    outdated[0] = outdated[0][2:] # This line is so ugly, but this is all I can do.
-
-    for pkg in outdated:
-        try:
-            call(['pip3', 'install', pkg, '--no-cache-dir', '--upgrade'])
-            print('updating...')
-        except Exception as e:
-            print(e)
-            print('Can not upgrade package %s' % pkg)
-            continue
-    print('Auto upgrade successful.')
+for dist in pip.get_installed_distributions():
+    call("pip3 install --upgrade --no-cache-dir" + dist.project_name, shell=True)
