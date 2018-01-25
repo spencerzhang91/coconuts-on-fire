f0cf509c752f3d01b52e840a2d832a1596e1e079
スペンサー <spencerpomme@live.com>
2018-01-25 12:33:28 +0800

    push

---
+++ isBST.c
@@ -0,0 +1,21 @@
+bool isBST(struct node* root)
+{
+    static struct node *prev = NULL;
+
+    // traverse the tree in inorder fashion and keep track of prev node
+    if (root)
+    {
+        if (!isBST(root->left))
+          return false;
+
+        // Allows only distinct valued nodes
+        if (prev != NULL && root->data <= prev->data)
+          return false;
+
+        prev = root;
+
+        return isBST(root->right);
+    }
+
+    return true;
+}
